8dcfd2a85af16f4331b5e6c9b7d4bffc3a712f23
Daniel <daniel@inventti.com.br>
2014-04-11 12:40:25 -0300

    DJ:Criando o game

---
+++ Games/tutorial - teste/tutorial_step3/js/resources.js
@@ -0,0 +1,29 @@
+game.resources = [
+	/**
+	 * Graphics.
+	 */
+	// our level tileset
+	{name: "mapa_tiles",  type:"image",	src: "data/img/map/mapa_tiles.png"},
+
+	{name: "mapa_tiles_2",  type:"image",	src: "data/img/map/mapa_tiles_2.png"},
+	// the main player spritesheet
+	{name: "gripe_run_right",     type:"image",	src: "data/img/sprite/gripe_run_right.png"},
+	// the spinning coin spritesheet
+    {name: "spinning_coin_gold",  type:"image", src: "data/img/sprite/spinning_coin_gold.png"},
+
+    // our enemty entity
+    {name: "wheelie_right",       type:"image", src: "data/img/sprite/wheelie_right.png"},
+     
+	/* 
+	 * Maps. 
+ 	 */
+	{name: "mapa",              type: "tmx",	src: "data/map/mapa.tmx"}
+
+	/* 
+	 * Background music. 
+	 */	
+	
+	/* 
+	 * Sound effects. 
+	 */
+];
